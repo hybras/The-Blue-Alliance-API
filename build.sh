c5260443f1ac501b152c2ccbbaf9fab6827081fa
hybras <24651269+hybras@users.noreply.github.com>
2021-06-09 17:47:34 -0400

    Clean build script
    * only update changed files
    * use pwd instead of $PWD
    * download spec file on demand

--- build.sh
+++ build.sh
@@ -1,17 +1,22 @@
+#!/usr/bin/env sh
+
 # Download Spec
-# curl 'https://www.thebluealliance.com/swagger/api_v3.json' -o spec.json
+curl 'https://www.thebluealliance.com/swagger/api_v3.json' -o spec.json
 
 # Generate project
 docker run --rm \
-    -v $PWD:/local \
+    -v $(pwd):/local \
     -u $UID:$UID \
     openapitools/openapi-generator-cli generate \
         --input-spec '/local/spec.json' \
         --generator-name 'kotlin' \
         --output '/local/'  \
+        --minimal-update \
         --package-name 'com.thebluealliance' \
         --group-id 'com.thebluealliance' \
         --artifact-id 'api' \
+
+rm spec.json
 
 # Add github pages jekyll config to docs
 cp jekyll.yml docs/_config.yml
